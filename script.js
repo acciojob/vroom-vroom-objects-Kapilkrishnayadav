7ea290c5a88eb105022b292ab91649be5d11e692
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-26 17:48:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-// Complete the js code
-function Car(make, model) {}
+// Base Car class
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
 
-function SportsCar(make, model, topSpeed) {}
+  getMakeModel() {
+    return `${this.make} ${this.model}`;
+  }
+}
 
-// Do not change the code below
+// SportsCar class inheriting from Car
+class SportsCar extends Car {
+  constructor(make, model, topSpeed) {
+    // Call the parent class constructor
+    super(make, model);
+    this.topSpeed = topSpeed;
+  }
+
+  getTopSpeed() {
+    return this.topSpeed;
+  }
+}
+
+// Example Usage:
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+console.log(car.getMakeModel()); // Ferrari Testarossa
+console.log(car.getTopSpeed());  // 200
+
 window.Car = Car;
 window.SportsCar = SportsCar;
